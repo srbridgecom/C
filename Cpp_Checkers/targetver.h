e202ffb667d72ec629270ea71ae45c582642ea8e
R.J. Bridge <ryanjbcom@users.noreply.github.com>
2019-06-17 08:05:13 -0700

    Create targetver.h

---
+++ Cpp_Checkers/targetver.h
@@ -0,0 +1,20 @@
+/* Category: C++
+Published on Tuesday, 13 May 2011 00:10
+Written by Ryan Bridglal
+Hits: 0
+Original Code posted @ GONE
+Ryan J. Bridglal,  
+November 2011
+*/
+
+// checkers.cpp : Defines the entry point for the console application.
+//
+
+#pragma once
+
+// Including SDKDDKVer.h defines the highest available Windows platform.
+
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+
+#include <SDKDDKVer.h>
