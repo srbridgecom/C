4032795e3402e5eab8515cf57d9e259cda0d7926
R.J. Bridge <ryanjbcom@users.noreply.github.com>
2019-06-17 08:04:18 -0700

    Create stdafx.h

---
+++ Cpp_Checkers/stdafx.h
@@ -0,0 +1,23 @@
+/* Category: C++
+Published on Tuesday, 13 May 2011 00:10
+Written by Ryan Bridglal
+Hits: 0
+Original Code posted @ gone
+Ryan J. Bridglal,  
+November 2011
+*/
+
+// checkers.cpp : Defines the entry point for the console application.
+//
+
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
