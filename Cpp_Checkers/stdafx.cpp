1c9aa454689f718c61482cba3bc795d3a0eeb952
R.J. Bridge <ryanjbcom@users.noreply.github.com>
2019-06-17 07:56:14 -0700

    Create stdafx.cpp

---
+++ Cpp_Checkers/stdafx.cpp
@@ -0,0 +1,17 @@
+/* Category: C++
+Published on Tuesday, 13 May 2011 00:10
+Written by Ryan Bridglal
+Hits: 0
+Original Code posted @ gone
+Ryan J. Bridglal,  
+November 2011
+*/
+
+// checkers.cpp : Defines the entry point for the console application.
+//
+
+// stdafx.cpp : source file that includes just the standard includes
+// checkers.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
